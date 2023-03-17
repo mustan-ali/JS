7964c24daccdc008b4e9884ed88eab51da6ba6a2
Mustan Ali <mustan9900@gmail.com>
2023-03-17 14:48:17 +0500

    Operator

---
+++ 03_Operator.js
@@ -0,0 +1,81 @@
+let a = 10
+let b = 3
+
+//Arithmetic Operators
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)     // a^b
+
+//Postfix and Prefix Increment/Decrement Operators
+console.log(a++)        // Print a then increment a
+console.log(a--)        // Print a then decrement a
+
+console.log(++a)        // Increment a then print a
+console.log(--a)        // Decrement a then print a
+console.log(" ")
+
+//Assignment Operators
+console.log(c = 10)       // c = 10
+console.log(c += 1)       // c = c + 5
+console.log(c -= 2)       // c = c - 5
+console.log(c *= 3)       // c = c * 5
+console.log(c /= 4)       // c = c / 5
+console.log(c %= 5)       // c = c % 5
+console.log(c **= 6)      // c = c ** 5
+
+let d = 5
+let e = '5'
+
+//Comparison Operators
+console.log(d == e)     //true
+console.log(d === e)    //false
+console.log(d != e)     //false
+console.log(d !== e)    //true
+console.log(d > e)      //false
+console.log(d < e)      //false
+console.log(d >= e)     //true
+console.log(d <= e)     //true
+
+
+/*
+Arithmetic Operators
++   (addition)
+-   (subtraction)
+*   (multiplication)
+/   (division)
+%   (modulus)
+++  (increment)
+--  (decrement)
+**  (exponentiation)
+
+
+//Assignment Operators
+=   (assignment)
++=  (addition assignment)
+-=  (subtraction assignment)
+*=  (multiplication assignment)
+/=  (division assignment)
+%=  (modulus assignment)
+**= (exponentiation assignment)
+
+
+//Comparison Operators
+==  (equal to)
+=== (strict equal to) also checks the data type
+!=  (not equal to)
+!== (strict not equal to)
+>   (greater than)
+<   (less than)
+>=  (greater than or equal to)
+<=  (less than or equal to)
+?   (ternary operator)
+
+
+//Logical Operators
+&&  (logical and) both conditions must be true
+||  (logical or) either condition must be true
+!   (logical not) reverses the result
+*/
