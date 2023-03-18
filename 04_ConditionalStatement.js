a37f486b4fd2e93d9d86644a33d628c301d928a9
Mustan Ali <mustan9900@gmail.com>
2023-03-18 17:14:51 +0500

    Conditional Statements

---
+++ 04_ConditionalStatement.js
@@ -0,0 +1,31 @@
+let color = 'Red'
+
+//IF Statement
+if (color == 'Red') {
+    console.log("Stop")
+}
+
+
+//IF-ELSE Statement
+if (color == 'Red') {
+    console.log("Stop")
+}
+else {
+    console.log("Go")
+}
+
+
+//IF-ELSEIF-ELSE Statement
+if (color == 'Red') {
+    console.log("Stop")
+}
+else if (color == 'Yellow') {
+    console.log("Ready")
+}
+else {
+    console.log("Go")
+}
+
+
+//Ternary Operator
+let message = (color == 'Red') ? "Stop" : "Go"
