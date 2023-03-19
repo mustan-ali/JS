5a8548f51226067846eccd17985df0eccea99f83
Mustan Ali <mustan9900@gmail.com>
2023-03-19 10:52:34 +0500

    Functions

---
+++ 06_Function.js
@@ -0,0 +1,27 @@
+//Function without parameters
+function sayHello() {
+    console.log('Hello');
+}
+sayHello();
+
+
+//Function with parameters
+function greet(name) {
+    console.log('Hello ' + name);
+}
+greet('John');
+
+
+//Function with return value
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
+
+/*
+Type of function
+No Parameters and no Return value
+No Parameters and Return value
+Parameters and no Return value
+Parameters and Return value
+*/
