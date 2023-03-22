421e3ca39c22c1a4e8d3fd6c81723306329bc8ba
Mustan Ali <mustan9900@gmail.com>
2023-03-22 15:47:48 +0500

    Console Object

---
+++ 09_ConsoleObject.js
@@ -0,0 +1,20 @@
+console.clear();
+console.log('Hello World');
+console.error('This is an error');
+console.assert(5 > 10, '5 is not greater than 10');
+console.table({ name: 'John', age: 20 });
+console.info('This is an information');
+console.time('Time Taken');
+console.timeEnd('Time Taken');
+
+/*
+log() - used to print the message in the console
+error() - used to print the error message in the console
+warn() - used to print the warning message in the console
+assert() - used to print the message if the condition is false
+clear() - used to clear the console
+table() - used to print the data in the tabular format
+info() - used to print the information message in the console
+time() - used to start the timer
+timeEnd() - used to end the timer and print the time taken
+*/
