dba954197452dcf9962cffd692d8c19d6bf94824
Mustan Ali <mustan9900@gmail.com>
2023-03-23 13:14:19 +0500

    Alert, Prompt, Confirm

---
+++ 10_AlertPromptConfirm.js
@@ -0,0 +1,16 @@
+alert("This is an alert")
+prompt("This is a prompt")
+confirm("This is a confirm")
+
+
+let name = prompt("Enter your name")
+let age = prompt("Enter your age", 18)  //prompt with default value 
+confirm("Are you sure you want to submit?")
+alert("Thank you for submitting")
+
+
+/*
+alert() - used to print the message in the alert box
+prompt() - used to print the message in the prompt box and also used to take the input from the user
+confirm() - used to print the message in the confirm box
+*/
