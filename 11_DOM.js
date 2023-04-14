f6188ad5170c8ab1772e557e4d966712bcab6ce2
Mustan Ali <mustan9900@gmail.com>
2023-04-14 13:19:32 +0500

    DOM

--- 11_DOM.js
+++ 11_DOM.js
@@ -6,6 +6,20 @@ document.getElementsByName("name"); // returns a list of elements with the given
 document.getElementById("id").innerHTML = "new text"; // changes the inner HTML of the element with the given id
 document.getElementById("id").style.color = "red"; // changes the style of the element with the given id
 
+document.querySelector("selector"); // returns the first element that matches the given CSS selector
+document.querySelectorAll("selector"); // returns a list of elements that matches the given CSS selector
+document.querySelector(".selector"); // returns the first element that matches class selector
+document.querySelector("#selector"); // returns the first element that matches id selector
+
+document.querySelector("element.class"); // returns the first element that matches class selector inside element
+document.querySelector("element,element"); // returns the first element that matches either of the selectors
+document.querySelector("element1 element2"); // returns the first element that have element2 inside element1
+document.querySelector("element1 > element2"); // returns the first element that have element2 as a direct child of element1
+document.querySelector("element1 + element2"); // returns the first element that have element2 as a direct sibling of element1 
+
+document.querySelectorAll(".selector")[0]; // returns the first element that matches class selector (alternative to querySelector)
+document.querySelector(".text").style.color = "red"; // changes the style of the first element that matches class selector
+
 
 /*
 firstChild  - returns the first child of a node
