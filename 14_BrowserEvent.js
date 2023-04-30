16beec0116dace4e0232625bef0996485ed8f509
Mustan Ali <mustan9900@gmail.com>
2023-04-30 12:12:45 +0500

    Browser Event

---
+++ 14_BrowserEvent.js
@@ -0,0 +1,12 @@
+//Mouse Events
+onclick //when the mouse clicks on an element
+ondblclick //when the mouse double-clicks on an element
+onmousedown //when the mouse button is pressed down on an element
+onmouseup //when the mouse button is released over an element
+onmouseover //when the mouse pointer moves over an element
+onmouseout //when the mouse pointer moves out of an element
+onmousemove //when the mouse pointer moves while it is over an element
+
+//Keyboard Events
+onkeydown //when a user is pressing/holding down a key
+onkeyup //when a user releases a key
