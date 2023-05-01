8906c5614ad42f68e2784912945189e5e3b11d5e
Mustan Ali <mustan9900@gmail.com>
2023-05-01 11:56:54 +0500

    Browser Event

--- 14_BrowserEvent.js
+++ 14_BrowserEvent.js
@@ -10,3 +10,14 @@ onmousemove //when the mouse pointer moves while it is over an element
 //Keyboard Events
 onkeydown //when a user is pressing/holding down a key
 onkeyup //when a user releases a key
+
+
+//Event Handlers
+
+let btn = document.getElementById("btn");
+let x = function () {
+    alert("Hello World");
+}
+
+btn.onclick = addEventListener("click", x);    //add event listener to the button & call the function x
+btn.onclick = removeEventListener("click", x); //remove event listener from the button & remove the function x
