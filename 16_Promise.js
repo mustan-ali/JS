59d89ca739a347a6ca327206513c7bd2bf64ef8f
Mustan Ali <mustan9900@gmail.com>
2023-06-29 15:31:23 +0500

    Promise

--- 16_Promise.js
+++ 16_Promise.js
@@ -2,3 +2,21 @@
 //Promise has 2 properties: state and result
 //state: initially pending, then either fulfilled when resolve is called or rejected when reject is called
 //result: initially undefined, then value if fulfilled or error if rejected
+
+let promise = new Promise(function (resolve, reject) {
+    let x = 10;
+    if (x == 10) {
+        resolve("Success");
+    }
+    else {
+        reject("Error");
+    }
+});
+
+//.then() is called when promise is resolved and .catch() is called when promise is rejected
+promise.then(function (message) {
+    console.log(message);
+}
+).catch(function (message) {
+    console.log(message);
+});
