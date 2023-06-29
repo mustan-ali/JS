9a6fbc5ab62a0c20f23a79b7b39a262e6d3422de
Mustan Ali <mustan9900@gmail.com>
2023-06-29 15:02:12 +0500

    Promise

---
+++ 16_Promise.js
@@ -0,0 +1,4 @@
+//Promise is an object representing the eventual completion or failure of an asynchronous operation.
+//Promise has 2 properties: state and result
+//state: initially pending, then either fulfilled when resolve is called or rejected when reject is called
+//result: initially undefined, then value if fulfilled or error if rejected
