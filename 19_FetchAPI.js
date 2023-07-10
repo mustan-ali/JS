a9eedb3a741b99be16ab5777cfcaf006a35f140a
Mustan Ali <mustan9900@gmail.com>
2023-07-10 12:25:23 +0500

    POST request

--- 19_FetchAPI.js
+++ 19_FetchAPI.js
@@ -1,6 +1,6 @@
 //Fetch API is used to get resources from the server. It is a promise based API.
 
-let p = fetch("https://fakerapi.it/api/v1/texts?_quantity=1&_characters=50")
+let p = fetch("https://jsonplaceholder.typicode.com/todos/")
 p.then((response) => {
     console.log(response.status)
     console.log(response.ok)
@@ -8,3 +8,36 @@ p.then((response) => {
 }).then((response) => {
     console.log(response)
 })
+
+
+//Get Request using Async Await
+const getTodo = async (id) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/" + id)
+    const data = await response.json()
+    console.log(data)
+}
+
+getTodo(3)
+
+
+//Post Request using Async Await
+const addTodo = async (todo) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/", {
+        method: "POST",
+        body: JSON.stringify(todo),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+    const data = await response.json()
+    console.log(data)
+}
+
+addTodo({
+    userId: 1,
+    title: "New Todo",
+    completed: false,
+})
+
+
+
